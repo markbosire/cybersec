dc8460e7345a3dfbbe85e7c64039ad18163e2cc3
markbosire <83925855+markbosire@users.noreply.github.com>
2023-11-14 18:22:56 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,17 +18,17 @@ function Home() {
     {!loginToken?<div className='Home-div'>
     <Navbar loginToken={loginToken} />
     <div className="Home">
+      <section>
        
       <h2>Become a Cybersecurity Specialist Through Challenges</h2>
    
       <p>Improve your cybersecurity skills by training with continuous challenges and practice.</p>
-      <div className='button'onClick={() => {
-                window.location.href = '/Join';
-              }}>Get Started</div>
+      <div className='button'>Get Started</div>
       <div className='InfoCardContainer'>
         <InfoCard text={"Solve security problems in he browser immediately with our built in system. We use containers to run commands on he spot."} logo={wrld} image={hacker}/>
         <InfoCard text={"Earn ranks by unlocking missions from beginner to experts. Do daily and weekly challenges to rank higher in the leaderboard."} logo={up} image={lvlup}/>
       </div>
+      </section>
     </div>
     </div>:<HomeContent loginToken={loginToken}/>}
     </React.Fragment>
